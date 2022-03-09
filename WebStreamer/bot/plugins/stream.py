5d2b3022ff8d49b8ef11c722c836a4fad7ecb8c0
JonaxHS <75236163+JonaxHS@users.noreply.github.com>
2022-03-09 14:31:44 -0600

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -89,7 +89,7 @@ async def private_receive_handler(c: Client, m: Message):
             text=msg_text.format(file_name, file_size, stream_link),
             parse_mode="HTML", 
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Descarga ahora 📥", url=http://mistream.rf.gd/video.php?u=stream_link&t=file_name)]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ 📥", url=http://mistream.rf.gd/video.php?u=stream_link)]])
             quote=True
         )
     except FloodWait as e:
